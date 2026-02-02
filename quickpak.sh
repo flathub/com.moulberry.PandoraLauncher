01e3322ce72cfd86d4890f038af627e38ecb98ba
illy <fluoriteByte@disroot.org>
2026-02-02 11:49:53 +0100

    ~temp workaround for flatpak not recognizing valid categories + added validation to quickpak script & make it reusable for later projects

--- quickpak.sh
+++ quickpak.sh
@@ -2,10 +2,14 @@
 # just to script to help me not have to ctrl + r the command lol
 set -x
 FPBUILD=
+FLATPAK_MANIFEST_NAME=com.moulberry.PandoraLauncher.yaml
 if command -v flatpak-builder; then
   FPBUILD=$(command -v flatpak-builder)
 else
   FPBUILD="flatpak run org.flatpak.Builder"
 fi
 
-$FPBUILD --force-clean build --repo=repo com.moulberry.PandoraLauncher.yaml --user --mirror-screenshots-url=https://dl.flathub.org/media --install --ccache
+$FPBUILD --force-clean build --repo=repo "$FLATPAK_MANIFEST_NAME" --user --mirror-screenshots-url=https://dl.flathub.org/media --ccache
+# uncomment to build bundle
+# flatpak build-bundle repo release.flatpak "$FLATPAK_MANIFEST_NAME"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions --debug repo repo
